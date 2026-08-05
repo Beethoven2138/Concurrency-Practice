bd8d2d2f868a93da1fbfbf4212e22d5d7d1356a2
Saxon <saxon.supple@gmail.com>
2026-08-05 02:46:31 +0100

    Update ZeroEvenOdd.hpp

--- ZeroEvenOdd.hpp
+++ ZeroEvenOdd.hpp
@@ -1,18 +1,11 @@
-#pragma once
-#include <iostream>
-#include <mutex>
-#include <thread>
-#include <condition_variable>
-
 class ZeroEvenOdd {
 private:
     int n;
     bool print_odd = false;
     bool print_even = false;
     std::atomic<bool> finished = false;
-    std::mutex odd_mtx;
+    std::mutex mtx;
     std::condition_variable odd_done;
-    std::mutex even_mtx;
     std::condition_variable even_done;
 
 public:
@@ -22,27 +15,26 @@ public:
 
     // printNumber(x) outputs "x", where x is an integer.
     void zero(function<void(int)> printNumber) {
-        std::unique_lock<std::mutex> odd_lock(odd_mtx);
-        std::unique_lock<std::mutex> even_lock(even_mtx);
+        std::unique_lock<std::mutex> lock(mtx);
         while (!finished){
             printNumber(0);
             print_odd = true;
             odd_done.notify_one();
-            odd_done.wait(odd_lock, [&]{return !print_odd;});
+            odd_done.wait(lock, [&]{return !print_odd;});
             print_even = true;
             if (!finished)
                 printNumber(0);
             even_done.notify_one();
             if (finished)
                 break;
-            even_done.wait(even_lock, [&]{return !print_even;});
+            even_done.wait(lock, [&]{return !print_even;});
         }
     }
 
     void even(function<void(int)> printNumber) {
-        std::unique_lock<std::mutex> even_lock(even_mtx);
+        std::unique_lock<std::mutex> lock(mtx);
         for (int i = 2; i <= n; i += 2){
-            even_done.wait(even_lock, [&]{return print_even;});
+            even_done.wait(lock, [&]{return print_even;});
             printNumber(i);
             print_even = false;
             if (i == n)
@@ -52,9 +44,9 @@ public:
     }
 
     void odd(function<void(int)> printNumber) {
-        std::unique_lock<std::mutex> odd_lock(odd_mtx);
+        std::unique_lock<std::mutex> lock(mtx);
         for (int i = 1; i <= n; i += 2){
-            odd_done.wait(odd_lock, [&]{return print_odd;});
+            odd_done.wait(lock, [&]{return print_odd;});
             printNumber(i);
             print_odd = false;
             if (i == n)
